a27438b97bedd901518380801f9b1d75279b4db3
casseylim <casseylim@hotmail.com>
2026-08-26 15:50:23 +0800

    Add account session version migration

---
+++ backend/StackMeet.Api/Migrations/20260826074651_AddAccountSessionVersion.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StackMeet.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountSessionVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "SessionVersion",
+                schema: "dbo",
+                table: "AppUser",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 1L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SessionVersion",
+                schema: "dbo",
+                table: "AppUser");
+        }
+    }
+}
