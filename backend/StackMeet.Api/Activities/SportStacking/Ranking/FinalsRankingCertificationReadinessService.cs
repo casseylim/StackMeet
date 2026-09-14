4bc86d5a2db13ff44db3ca909b1040a8ab27e4b8
casseylim <casseylim@hotmail.com>
2026-09-14 13:05:34 +0800

    fix: import activity module contract for SP-4I

--- backend/StackMeet.Api/Activities/SportStacking/Ranking/FinalsRankingCertificationReadinessService.cs
+++ backend/StackMeet.Api/Activities/SportStacking/Ranking/FinalsRankingCertificationReadinessService.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
+using StackMeet.Api.Activities;
 using StackMeet.Api.Data;
 
 namespace StackMeet.Api.Activities.SportStacking.Ranking;
